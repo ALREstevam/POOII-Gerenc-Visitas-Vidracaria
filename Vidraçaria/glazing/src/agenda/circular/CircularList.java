faa61fd17daaa60a11a2af954f64aed1e5d90c3e
pedroArtico <pedro_artico@hotmail.com>
2017-11-15 09:32:39 -0200

    ...........

--- Vidraçaria/glazing/src/agenda/circular/CircularList.java
+++ Vidraçaria/glazing/src/agenda/circular/CircularList.java
@@ -118,80 +118,138 @@ public class CircularList<E> implements List<E>, Iterable<E>, Serializable{
         int rsp = (head + circularPos) % this.innerList.size(); 
         return Math.abs(rsp);
     }
-    
+    /**
+ * This method adds a header to the "e"
+ * @param e is an object from E
+ * @return boolean
+ */
     @Override
     public boolean add(E e) {
         advanceHead();
         this.innerList.add(head, e);
         return true;
     }
-    
+    /**
+ * This method gets a position from the circular list
+ * @param index is an index of int type
+ * @return E
+ */    
     @Override
     public E get(int index) {
         return this.innerList.get(this.circularPosToArrayPos(index));
     }
-    
+    /**
+ *This method sets a position of object
+ * @param index is an index of int type
+ * @param element is an object from E
+ * @return E
+ */        
     @Override
     public E set(int index, E element) {
         return this.innerList.set(this.circularPosToArrayPos(index), element);
     }
-    
+    /**
+ *This method adds an element to the circular list
+ * @param index is an index of int type
+ * @param element is an object from E
+ */            
     @Override
     public void add(int index, E element) {
         this.innerList.add(this.circularPosToArrayPos(index), element);
     }
-       
+    /**
+    * This method aremoves an element to the circular list
+    * @param index is an index of int type
+    * @return E 
+ */                   
     @Override
     public E remove(int index) {
          return this.innerList.remove(this.circularPosToArrayPos(index));
     }
-     
+    /**
+ * This method obtains the index of an object
+ * @param o is an index of int type
+ * @return int 
+ */                 
     @Override
     public int indexOf(Object o) {
         return this.arrayPosToCircularPos(this.innerList.indexOf(o));
     }
-    
+    /**
+ * This method obtains the size of list
+ * @return int
+ */    
     @Override
     public int size() {
         return this.innerList.size();
     }
-    
+ /**
+ * This method check if the list is empty
+ * @return boolean
+ */   
     @Override
+     
     public boolean isEmpty() {
         return this.innerList.isEmpty();
     }
-    
+    /**
+ * This method check if the list contains an object
+     * @param o  is the object
+ * @return boolean
+ */        
     @Override
     public boolean contains(Object o) {
         return this.innerList.contains(o);
     }
-    
+    /**
+ * This method removes an object from list
+     * @param o is the object
+ * @return boolean
+ */            
     @Override
     public boolean remove(Object o) {
         return this.innerList.remove(o);
     }
-    
+    /**
+    * Convert to Array
+    * @return Object
+    */    
     @Override
     public Object[] toArray() {
         return this.innerList.toArray();
     }
-
+    /**
+    * Converts to Array
+     * @param <T> has a aray of objects
+     * @param a is an object
+    * @return Object
+    */    
     @Override
     public <T> T[] toArray(T[] a) {
         return this.innerList.toArray(a);
     }
-    
+    /**
+    * Is a sublist
+     * @param fromIndex inicial index
+     * @param toIndex from which index
+    * @return List
+    */    
     @Override
     public List<E> subList(int fromIndex, int toIndex) {
         return this.innerList.subList(this.circularPosToArrayPos(fromIndex), this.circularPosToArrayPos(toIndex));
     }
-    
+    /**
+    * This method assigns the value 0 to the head. This method clears the list
+    */    
     @Override
     public void clear() {
        this.head = 0;
        this.innerList.clear();
     }
-    
+    /**
+    * This method is an iterator
+    * @return Iterator
+    */ 
     
     @Override
     public Iterator<E> iterator() {
@@ -209,7 +267,11 @@ public class CircularList<E> implements List<E>, Iterable<E>, Serializable{
     public ListIterator<E> listIterator(int index) {
         throw new UnsupportedOperationException("Not supported."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    /**
+    * Convert an object to String. Gets a textual representation of 
+    * a specific object.
+    * @return String
+    */ 
     @Override
     public String toString() {
         String rsp = "";
@@ -272,7 +334,10 @@ public class CircularList<E> implements List<E>, Iterable<E>, Serializable{
     
     private class CircularListIterator<E> implements Iterator<E>{
         private int position = 0;
-        
+        /**
+     * This method verify if has a next element
+     * return boolean
+     */
         @Override
         public boolean hasNext() {
             return position > arrayPosToCircularPos(head - 1);
