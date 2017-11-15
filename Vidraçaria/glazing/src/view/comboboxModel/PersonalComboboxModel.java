0eff179f9839d717f1fdc4e64ab71f5d478688ed
pedroArtico <pedro_artico@hotmail.com>
2017-11-15 10:53:19 -0200

    .

--- Vidraçaria/glazing/src/view/comboboxModel/PersonalComboboxModel.java
+++ Vidraçaria/glazing/src/view/comboboxModel/PersonalComboboxModel.java
@@ -19,7 +19,10 @@ public class PersonalComboboxModel<E extends Descriptible> extends AbstractListM
     private List<E> elemList;
     private String separator;
     private E selected = null; /*(elemList != null && elemList.size() > 0)?elemList.get(0):null;*/
-    
+/**
+ * Obtains the size of list
+ * @return int
+ */    
     @Override
     public int getSize() {
         if(this.elemList == null){
@@ -28,24 +31,37 @@ public class PersonalComboboxModel<E extends Descriptible> extends AbstractListM
             return this.elemList.size();
         }
     }
-
+    /**
+    * Gets an element at
+    * @param index is an index of list
+    * @return int
+    */    
     @Override
     public E getElementAt(int index) {
         return this.elemList.get(index);
     }
-
+    /**
+    * Sets the item that is selected 
+    * @param anItem is an object
+    */    
     @Override
     public void setSelectedItem(Object anItem) {
         System.out.println("SELECTED!");
         this.selected = (E)anItem;
         fireContentsChanged(this.elemList, 0, this.elemList.size());
     }
-
+    /**
+    * Gets the item that is selected 
+    * @return Object
+    */    
     @Override
     public Object getSelectedItem() {
         return this.selected;
     }
-    
+    /**
+    * Adds an element to the list 
+    * @return element
+    */    
     public boolean add(E element){
         this.elemList.add(element);
         fireContentsChanged(this.elemList, 0, this.elemList.size());
@@ -60,13 +76,19 @@ public class PersonalComboboxModel<E extends Descriptible> extends AbstractListM
     public void add(List<E> elemList){
         this.elemList = elemList;
     }
-    
+    /**
+    * Removes an element of list 
+    * @return boolean
+    */    
     public boolean remove(E element){
         this.elemList.remove(element);
         fireContentsChanged(this.elemList, 0, this.elemList.size());
         return true;
     }
-    
+    /**
+    * Cleans an element of list 
+    * @param element is a element of list
+    */    
     public void clear(E element){
         this.elemList.clear();
         fireContentsChanged(this.elemList, 0, this.elemList.size());
